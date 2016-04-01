b89f067c913801dabe9f1a9c15cb33dd6217a4a6
Camilla Borg <camillaborg88@gmail.com>
2016-04-01 17:02:22 +0200

    More styling to case boxes

--- front-page.php
+++ front-page.php
@@ -25,6 +25,8 @@ $content = get_the_content();
                 $header = get_sub_field('header');
                 $video = get_sub_field('video');
                 $radio = get_sub_field('radio');
+                $banner = get_sub_field('banner');
+                $ad = get_sub_field('ad');
 
                 ?>
 
@@ -48,13 +50,27 @@ $content = get_the_content();
                         <?php if( $video ): ?>
                         <div class="embed-container"><?php echo $video ?></div>
                         <?php endif; ?>
-                            <?php if ($radio):?>
-                                <audio controls class="audio">
-                                    <source src="<?php echo $radio['url']; ?>" type="audio/wav">
-                                </audio>
-                            <?php endif; ?>
                         </div>
                     </div>
+
+                <div class="row container">
+                    <div class="six columns item image">
+                        <?php if( $banner ): ?>
+                            <img src="<?php echo $banner['url']; ?>" alt="<?php echo $banner['alt'] ?>" class="case-img" />
+                        <?php endif; ?>
+                        <?php if ($radio):?>
+                            <audio controls class="audio">
+                                <source src="<?php echo $radio['url']; ?>" type="audio/wav">
+                            </audio>
+                        <?php endif; ?>
+                    </div>
+                        <div class="six columns item image">
+                            <?php if( $ad ): ?>
+                                <img src="<?php echo $ad['url']; ?>" alt="<?php echo $ad['alt'] ?>" class="case-img ad" />
+                            <?php endif; ?>
+                        </div>
+                </div>
+
             </div>
 
 
